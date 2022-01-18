aa1872054f97fef1a0362aa1000afd3cad281df6
Simon Palm <sp222ym@student.lnu.se>
2022-01-18 14:11:56 +0100

    Adding scroll and routes.

--- Client/src/container/Home.jsx
+++ Client/src/container/Home.jsx
@@ -14,6 +14,8 @@ const Home = () => {
 
     const [toggleSidebar, setToggleSidebar] = useState(false);
     const [user, setUser] = useState(null)
+    const scrollRef = useRef(null);
+
     const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user') ) : localStorage.clear()
 
     useEffect(() => {
@@ -25,6 +27,10 @@ const Home = () => {
         })
     }, []);
 
+    useEffect(() => {
+       scrollRef.current.scrollTo(0, 0)
+    }, [])
+
     return (
         <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
             <div className='hidden md:flex h-screen flex-initial'>
@@ -32,6 +38,8 @@ const Home = () => {
             </div>
 
             <div className='flex md:hidden flex-row'>
+
+                <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
                 <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
                 <Link to='/'>
                     <img src={logo} alt='logo' className='w-28' />
@@ -39,7 +47,7 @@ const Home = () => {
                 <Link to={`user-profile/${user?._id}`}>
                     <img src={user?.image} alt='logo' className='w-28' />
                 </Link>
-            </div>
+                </div>
             {toggleSidebar && (
                 <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'> 
                     <div className='absolute w-full flex justify-end items-center p-2'> 
@@ -48,6 +56,16 @@ const Home = () => {
                     <Sidebar user={user && user } closeToggle={setToggleSidebar} />
                 </div>
             )}
+            </div>
+            
+            <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+                <Routes>
+                    <Route path='/user-profile/:userId' element={<UserProfile/>} />
+                    <Route path='/*' element={<Pins user={user && user} /> }  />
+
+    
+                </Routes>
+            </div>
         </div>
     )
 }
