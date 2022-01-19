61785fdba9ea23d1c2fc2366c56b6d471e5f3688
Palmen98 <simon.palm98@hotmail.se>
2022-01-19 08:50:45 +0100

    Adding imports, html, and variables.

--- Client/src/components/Pin.jsx
+++ Client/src/components/Pin.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import { urlFor } from '../client'
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {v4 as uuidv4} from 'uuid';
+import {MdDownloadForOffline} from 'react-icons/md';
+import {AiTwotoneDelete} from 'react-icons/ai';
+import {BsFillArrowUprightCircleFill} from 'react-icons/bs';
+
+import { urlFor, client } from '../client';
 
 const Pin = ({pin: {postedBy, image, _id, destination}}) => {
+
+    const [postHovered, setPostHovered] = useState(false);
+    const [savingPost, setSavingPost] = useState(false);
+
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <div className='m-2'>
+            <div onMouseEnter={() => setPostHovered(true)}
+            onMouseLeave={() => setPostHovered(false)}
+            onClick={() => navigate(`/pin/-detail/${_id}`)}
+            >
+
+            </div>
+
             <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
         </div>
     )
