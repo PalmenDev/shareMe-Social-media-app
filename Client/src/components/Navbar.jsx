563d730445eb8c5b3969fb8c4bc9270d639b7f84
Palmen98 <simon.palm98@hotmail.se>
2022-01-20 11:37:19 +0100

    Fixing a typo with the wrong linking.

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar({searchTerm, setSearchTerm, user }) {
                 <img src={user.image} alt='user-image' className='w-14 h-12 rounded-lg'/>
                 </Link>
 
-                <Link to='create.pin'
+                <Link to='create-pin'
                 className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
                 >
                 <IoMdAdd />
