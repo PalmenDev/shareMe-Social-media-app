ab05e02de2d684c318dad975c14e6b7fb6bc79e6
Palmen98 <simon.palm98@hotmail.se>
2022-01-20 10:55:31 +0100

    importing fetchUser.

--- Client/src/container/Home.jsx
+++ Client/src/container/Home.jsx
@@ -8,6 +8,7 @@ import { Sidebar, UserProfile } from '../components';
 import Pins from './Pins.jsx'
 import {client} from '../client.js';
 import logo from '../assets/logo.png';
+import { fetchUser } from '../utils/FetchUser';
 
  
 const Home = () => {
@@ -16,7 +17,7 @@ const Home = () => {
     const [user, setUser] = useState(null)
     const scrollRef = useRef(null);
 
-    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user') ) : localStorage.clear()
+    const userInfo = fetchUser();
 
     useEffect(() => {
         const query = userQuery(userInfo?.googleId);
